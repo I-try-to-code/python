9cd2fe2dbf0eee0c277c05858ebeb67e97618993
Ayan Gattani <ayangattani@gmail.com>
2025-05-09 18:44:28 +0530

    creating a random walk that randomly selects its direction and colour.

--- day_18/main.py
+++ day_18/main.py
@@ -1,18 +1,24 @@
 from turtle import Turtle, Screen
-
+import random
 timmy= Turtle()
 
 
 screen= Screen()
 timmy.shape("turtle")
 timmy.color("blue")
-import heroes
-print (heroes.gen())
-for i in range(7):
-     timmy.forward(10)
-     timmy.penup()
-     timmy.forward(10)
-     timmy.pendown()
-
-
+screen.colormode(255)
+def random_walk():
+     while True:
+          x= random.randint(0,1)
+          red = random.randint(0, 225)
+          blue = random.randint(0, 225)
+          green = random.randint(0, 225)
+          if x==0:
+               timmy.left(90)
+               timmy.forward(20)
+          else:
+               timmy.right(90)
+               timmy.forward(20)
+          timmy.color((red,blue,green))
+random_walk()
 screen.exitonclick()
