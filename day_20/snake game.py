e74888213ab7a254ca6e9703b3727d4b953d9a89
Ayan Gattani <ayangattani@gmail.com>
2025-05-16 12:42:05 +0530

    Added movement controls of snake

--- day_20/snake game.py
+++ day_20/snake game.py
@@ -1,35 +1,32 @@
 import time
-import turtle
 from turtle import Screen, Turtle
+from snake import Snake
+
 
 screen=Screen()
 screen.setup(600,600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-segm=[]
-pos=[(0,0),(-20,0),(-40,0)]
+
 screen.tracer(0)
+snake=Snake()
 
-for i in pos:
-    seg = Turtle()
-    seg.penup()
-    seg.shape("square")
-    seg.color("White")
-    seg.goto(i)
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.lt,"Left")
+screen.onkey(snake.rt,"Right")
 
-    segm.append(seg)
+screen.onkey(snake.up,"w")
+screen.onkey(snake.down,"s")
+screen.onkey(snake.lt,"a")
+screen.onkey(snake.rt,"d")
 
 
 sta=True
 while sta:
     time.sleep(0.2)
-    for i in range(len(segm)-1,0,-1):
-        new_x=segm[i-1].xcor()
-        new_y= segm[i - 1].ycor()
-        segm[i].goto(new_x,new_y)
-    segm[0].forward(20)
-    segm[0].left(20)
-
+    snake.move()
     screen.update()
 
 
